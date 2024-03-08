e819168f31655979015df66f2824f20bcb4e8943
gguinea 🦫 <kamil5176@gmail.com>
2024-03-08 01:30:02 +0100

    Money interface plus decimal impl

---
+++ internal/domain/money/money.go
@@ -0,0 +1,41 @@
+package money
+
+import "github.com/shopspring/decimal"
+
+type Money interface {
+	AddFromString(money string) (Money, error)
+	SubtractFromString(money string) (Money, error)
+	GetAsStringWithDefaultPrecision() string
+}
+
+type DecimalMoney struct {
+	amount decimal.Decimal
+}
+
+func NewDecimalMoney() DecimalMoney {
+	return DecimalMoney{}
+}
+
+const defaultPrecision = 2
+
+func (m *DecimalMoney) AddFromString(money string) (Money, error) {
+	amount, err := decimal.NewFromString(money)
+	if err != nil {
+		return nil, err
+	}
+	m.amount = m.amount.Add(amount)
+	return m, nil
+}
+
+func (m *DecimalMoney) SubtractFromString(money string) (Money, error) {
+	amount, err := decimal.NewFromString(money)
+	if err != nil {
+		return nil, err
+	}
+	m.amount = m.amount.Sub(amount)
+	return m, nil
+}
+
+func (m *DecimalMoney) GetAsStringWithDefaultPrecision() string {
+	return m.amount.StringFixedBank(defaultPrecision)
+}
---
+++ internal/domain/money/money_test.go
@@ -0,0 +1,94 @@
+package money
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestShouldAddMoneyProperly(t *testing.T) {
+	scenarios := []struct {
+		name           string
+		startingAmount string
+		amountToAdd    string
+		expectedAmount string
+	}{
+		{
+			name:           "Should add 0 to 0",
+			startingAmount: "0.00",
+			amountToAdd:    "0.00",
+			expectedAmount: "0.00",
+		},
+		{
+			name:           "Should add 1 to 0",
+			startingAmount: "0.00",
+			amountToAdd:    "1",
+			expectedAmount: "1.00",
+		},
+		{
+			name:           "Should add small to small",
+			startingAmount: "0.01",
+			amountToAdd:    "0.01",
+			expectedAmount: "0.02",
+		},
+		{
+			name:           "Should add big to big",
+			startingAmount: "1002.87",
+			amountToAdd:    "1232.13",
+			expectedAmount: "2235.00",
+		},
+	}
+
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			m := NewDecimalMoney()
+			m.AddFromString(scenario.startingAmount)
+			m.AddFromString(scenario.amountToAdd)
+			assert.Equal(t, scenario.expectedAmount, m.GetAsStringWithDefaultPrecision(), scenario.name)
+		})
+	}
+}
+
+func TestShouldSubtractMoneyProperly(t *testing.T) {
+	scenarios := []struct {
+		name              string
+		startingAmount    string
+		amountToSubtract  string
+		expectedAmount    string
+		expectedError     bool
+		expectedErrorText string
+	}{
+		{
+			name:             "Should subtract 0 from 0",
+			startingAmount:   "0.00",
+			amountToSubtract: "0.00",
+			expectedAmount:   "0.00",
+		},
+		{
+			name:             "Should subtract 1 from 0",
+			startingAmount:   "0.00",
+			amountToSubtract: "1",
+			expectedAmount:   "-1.00",
+		},
+		{
+			name:             "Should subtract small from small to be equal to 0",
+			startingAmount:   "0.01",
+			amountToSubtract: "0.01",
+			expectedAmount:   "0.00",
+		},
+		{
+			name:             "Should subtract big from big",
+			startingAmount:   "1002.87",
+			amountToSubtract: "1232.13",
+			expectedAmount:   "-229.26",
+		},
+	}
+	for _, scenario := range scenarios {
+		t.Run(scenario.name, func(t *testing.T) {
+			m := NewDecimalMoney()
+			m.AddFromString(scenario.startingAmount)
+			m.SubtractFromString(scenario.amountToSubtract)
+			assert.Equal(t, scenario.expectedAmount, m.GetAsStringWithDefaultPrecision(), scenario.name)
+		})
+	}
+}
